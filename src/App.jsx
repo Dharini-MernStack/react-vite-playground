18db639535007752ac31fc84365580765fe853e3
Dharini S <dharinisrinivasan.94@gmail.com>
2023-12-14 20:40:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { React,useState } from "react";
 
-function App() {
-	const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+function FavoriteColor() {
+ // Challenge 1 : Create a H! which renders the colour using a initial state set using set state
+ // Challenge 2: create a button that on click changes the state of the colour and renders the change in colour on click
 }
 
-export default App;
+
+
+              
